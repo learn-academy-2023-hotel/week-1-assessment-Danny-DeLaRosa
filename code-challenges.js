e4dc5390fea0a648780f34b5946a2e7167066464
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-15 16:45:25 -0700

    week 1 assesmemts complete

--- code-challenges.js
+++ code-challenges.js
@@ -27,19 +27,35 @@ const fruit2 = "banana"
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
-
+let padresWillWin2024WorldSeriesRuns = padres1984WorldSeriesRuns + padres1998WorldSeriesRuns
+// console.log(padresWillWin2024WorldSeriesRuns.length)
+// output: 16
 // Pseudo code:
+// Step 1: create a variable to combine the arrays padres1984WorldSeriesRuns & padres1998WorldSeriesRuns using addition
+// step 2 log the results using the .length modifier 
+// the output was far higher than i expected at 16. not sure what i did wrong here.
 
 // --------------------2) Create the code that will reverse the letters of a string using the test variable provided below.
 
 const currentCohort = "Golf 2023"
 // Expected output: "3202 floG"
-
+// console.log(currentCohort.reverse)
+// output: undefined
 // Pseudo code:
+// step 1: console log the variable currentCohort with the .reverse modifier.console
+// the output was undefined, i guess i forgot some steps or dont understand how this modifier works?
 
 // --------------------3) Create the code that will log only the odd numbers from the array using the test variable provided below.
 
 const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 // Expected output: 13 5 -5 27
-
+for(let i = [0]; i < stockExchange.length; i++){
+    if(stockExchange[i] % 2 !== 0)
+    console.log(stockExchange[i])
+}
 // Pseudo code:
+// step 1: create a for loop to make a list of the numbers in the array
+// step 2: add a conditional statement to the for loop that states that if the stockExchange index in the iteration is modulo by 2 the remainder should not be zero(otherwise the number is even)
+// the iteration should go down the line from 13 - 10 to see if the number divvided by two results in the remainder should not be zero, if it is, the number is even, if not, the number is odd.
+// console.log the stockExchange[i] so that the only numbers that populate are numbers that do not have a remainder of zero(odd numbers)
+// the output was 13 5 -5 27
--- coding-concepts.js
+++ coding-concepts.js
@@ -18,16 +18,16 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 9
+// b) Verify and explain: the string should have logged 10 because "LEARN 2023" has 10 characters including the space. i forgot to count the space in my answer smh
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: the output was "o" because that letter was the 4th one from the left if you start counting from 0 (0=H, 1=e, 2=l, and so o)
 
 // --------------------3) What will this log?
 
@@ -35,21 +35,21 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 3
+// b) Verify and explain: the output was Ruby. i thought that this was asking to tell me what the index of languages would be which now that i am thinking about it would nly be one since its one array right? anyways the answer is ruby because its the 1st string in the array if you counted from 0.
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I want to say it will uppercase Saturday & Suday ("SATURDAY, SUNDAY") but i feel like it may actually be an error or undefined.  I think you can only uppercase a string if im not mistaken and this is an array.
+// b) Verify and explain: should have went with my gut on this one. after doing research i verified that the .toUpperCase modifiers can only be used on strings.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Number???
+// b) Verify and explain: the output was number. i was not confident in my answer but the output was number because typeof logs what data type something is and the i knew that the .length modifier outputs a number. wasnt sure if an error was going to happen.
